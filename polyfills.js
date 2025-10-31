51698f8aa321210da371cc964955eafd22d46510
BuddyHoli <43436903+BuddyHoli@users.noreply.github.com>
2025-10-31 21:47:33 +0100

    Delete polyfills.js

--- polyfills.js
+++
@@ -1,8 +0,0 @@
-// src/polyfills.js
-// Polyfills für Browser/Capacitor WebView (Buffer für prismarine-nbt)
-import { Buffer } from "buffer";
-
-// make Buffer available globally (prismarine-nbt erwartet Node Buffer)
-if (typeof window !== "undefined" && !window.Buffer) {
-  window.Buffer = Buffer;
-}
